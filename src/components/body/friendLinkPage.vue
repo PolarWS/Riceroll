f556d47768dfb4b5da90ae4c59bfe5a582cff163
Forloen <forloen@outlook.com>
2023-11-05 00:24:25 +0800

    更新 friendLinkPage.vue

--- src/components/body/friendLinkPage.vue
+++ src/components/body/friendLinkPage.vue
@@ -1,8 +1,36 @@
 <template>
-    <h3>friendLinkPage</h3>
+    <div class="card_box">
+        <img class="card_img" />
+        <div class="card_title"></div>
+        <div class="card_content"></div>
+    </div>
 </template>
 <script>
-export default {}
+export default {
+    data(){
+        return{
+            cardList:[
+                {
+                    url:"",
+                    title:"",
+                    content:""
+                }
+            ]
+        }
+    }
+}
 </script>
 <style>
+    .card_box{
+        width: 50%;
+        height: 170px;
+        display: flex;
+        justify-content: space-between;
+        border: 2px solid red;
+        background-color: aqua;
+        border-radius: 15px;
+    }
+    .card_img{
+        border-radius: 50px;
+    }
 </style>
