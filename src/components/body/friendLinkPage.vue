7b0da4282fdedf703e1c5f3aa4a06ae1d015c3ab
Forloen <forloen@outlook.com>
2023-11-05 17:14:06 +0800

    友链1105晚更新

--- src/components/body/friendLinkPage.vue
+++ src/components/body/friendLinkPage.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="card_box" v-for="(card,index) in cardList">
-        <img class="card_img" :src= "(card.src)"/>
-        <div class="card-content">
-          <h2 class="title">{{ card.title }}</h2>
-          <p class="content">{{ card.content }}</p>
+    <div class="father_box" id="pressed">
+        <div class="card_box" v-for="(card, index) in cardList" :key="index">
+            <img class="card_img" :src="(card.src)" />
+            <div class="card-content">
+                <h3 class="title">{{ card.title }}</h3>
+                <p class="content">{{ card.content }}</p>
+                <div class="status-bar" :class="{ 'inaccessible': card.inaccessible }"></div>
+            </div>
         </div>
     </div>
 </template>
+
 <script>
 export default {
     data(){
@@ -15,17 +19,32 @@ export default {
                 {
                     src:"/src/components/img/zhuye.png",
                     title:"只是标题1",
-                    content:"看什么看，内容没见过啊！"
+                    content:"看什么看，内容没见过啊！",
+                    inaccessible:true
                 },
                 {
                     src:"/src/components/img/zhuye.png",
                     title:"2号标题",
-                    content:"看什么看，内容没见过啊！是不是没见过！是不是没见过！是不是没见过！是不是没见过！"
+                    content:"看什么看，内容没见过啊！是不是没见过！是不是没见过！是不是没见过！是不是没见过！",
+                    inaccessible:false
                 },
                 {
                     src:"/src/components/img/zhuye.png",
                     title:"怎么看也就只是标题啊",
-                    content:"看什么看，内容没见过啊！"
+                    content:"看什么看，内容没见过啊！",
+                    inaccessible:false
+                },
+                {
+                    src:"/src/components/img/zhuye.png",
+                    title:"怎么看也就只是标题啊对吧对吧",
+                    content:"看什么看，内容没见过啊！",
+                    inaccessible:false
+                },
+                {
+                    src:"/src/components/img/zhuye.png",
+                    title:"怎么看也就只是标题啊对吧对吧",
+                    content:"看什么看，内容没见过啊！是不是没见过！是不是没见过！是不是没见过！是不是没见过！！",
+                    inaccessible:false
                 }
             ]
         }
@@ -33,33 +52,41 @@ export default {
 }
 </script>
 <style>
-    .card_box{
-        margin: 20px;
-        width: 50%;
-        height: 150px;
+    .father_box {
         display: flex;
-        justify-content: space-between;
-        border: 2px solid red;
+        flex-wrap: wrap;
+    }
+
+    .card_box {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 20px 16px 0px 16px;
+        width: 45%;
+        height: auto;
         background-color: #F4F4F4;
         border-radius: 15px;
+        position: relative; /* Add this to make the status bar relative to the card */
     }
-    .card_img{
+
+    .card_img {
         margin: auto auto;
-        height: 100px;
-        width: 100px;
+        height: 85px;
+        width: 85px;
         border-radius: 50px;
     }
-    .card_content{
-        flex: 1;
-        margin-left: 10px
+
+    .card-content {
+        margin-left: 10px;
     }
-    .title{
-        width: 200px;
+
+    .title {
+        max-width: 190px;
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
     }
-    .content{
+
+    .content {
         height: 45px;
         width: 200px;
         word-break: break-all;
@@ -67,5 +94,20 @@ export default {
         display: -webkit-box;
         -webkit-line-clamp: 2;
         -webkit-box-orient: vertical;
+    }
+
+    .status-bar {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 10px;
+        background-color: aqua;
+        border-bottom-left-radius: 15px; 
+        border-bottom-right-radius: 15px; 
+    }
+
+    .status-bar.inaccessible {
+        background-color: red;
     }
 </style>
