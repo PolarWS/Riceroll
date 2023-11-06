b168bbfdca5ac0352be66ef9e4a02c121535ad1f
极地 <1677588395@qq.com>
2023-11-07 00:51:58 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import config from '../config.json'
 
-import navigationBar from '@/components/navigationBar.vue';
-import informationBar from '@/components/informationBar.vue';
-import articlePage from '@/components/body/articlePage.vue';
-import filePage from '@/components/body/filePage.vue';
-import aboutPage from '@/components/body/aboutPage.vue';
-import friendLinkPage from '@/components/body/friendLinkPage.vue';
-import musicPage from '@/components/body/musicPage.vue';
-import searchPage from '@/components/body/searchPage.vue';
+// import navigationBar from '@/components/navigationBar.vue';
+// import informationBar from '@/components/informationBar.vue';
+// import articlePage from '@/components/body/articlePage.vue';
+// import filePage from '@/components/body/filePage.vue';
+// import aboutPage from '@/components/body/aboutPage.vue';
+// import friendLinkPage from '@/components/body/friendLinkPage.vue';
+// import musicPage from '@/components/body/musicPage.vue';
+// import searchPage from '@/components/body/searchPage.vue';
 import bodyItem from '@/components/bodyItem.vue';
 
+const navigationBarData = config.navigationBarData;
 
-const componentMapping = {
-    homePage: articlePage, // 这里使用实际的组件对象
-    articlePage: articlePage, // 同样的，使用实际的组件对象
-    // 其他页面的映射
-};
+let defaultVue = ["articlePage", "searchPage", "musicPage", "filePage", "friendLinkPage", "aboutPage"];
+let pages = [];
 
-const pages = [{ path: '/', component: componentMapping[config.navigationBarData.titleData[config.navigationBarData.defaultSelected - 1].id] }];
+if (defaultVue.includes(navigationBarData.titleData[navigationBarData.defaultSelected - 1].id)) {
+    pages = [{ path: '/', component: () => import(`@/components/body/${navigationBarData.titleData[navigationBarData.defaultSelected - 1].id}.vue`) }];
+} else {
+    pages = [{ path: '/', component: bodyItem }];
+}
 
-for (const allocation of config.navigationBarData.titleData) {
-    pages.push({ path: '/' + allocation.id, component: componentMapping[allocation.id] });
+// 在此处可以使用 `pages`
+
+
+// 接下来可以在路由配置中使用`pages`
+
+
+// for (const allocation of navigationBarData.titleData) {
+//     if (defaultVue.includes(allocation.id)) {
+//         pages.push({ path: '/' + allocation.id, component: allocation.id });
+//     } else {
+//         pages.push({ path: '/' + allocation.id, component: bodyItem });
+//     }
+// }
+
+for (const allocation of navigationBarData.titleData) {
+    if (defaultVue.includes(allocation.id)) {
+        pages.push({ path: '/' + allocation.id, component: () => import(`@/components/body/${allocation.id}.vue`) });
+    } else {
+        pages.push({ path: '/' + allocation.id, component: bodyItem });
+    }
 }
 
 console.log(pages);
