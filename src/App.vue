663d992c101700f22627ab1c29be3fbcddf41742
极地 <1677588395@qq.com>
2023-11-07 11:28:32 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
   <navigationBar @handleClick="handleClick" :navigationBarData="navigationBarData" />
   <!-- 中心内容页 -->
   <div class="centralFramework">
-    <component :is="showBody" :itemData="itemData" v-if="showBodyDisplay"></component>
-    <bodyItem :itemData="itemData" v-if="!showBodyDisplay" />
+    <router-view></router-view>
   </div>
   <!-- 侧边栏 -->
   <informationBar :informationBarData="informationBarData" />
@@ -13,22 +12,10 @@
 <script>
 import navigationBar from '@/components/navigationBar.vue';
 import informationBar from '@/components/informationBar.vue';
-import articlePage from '@/components/body/articlePage.vue';
-import filePage from '@/components/body/filePage.vue';
-import aboutPage from '@/components/body/aboutPage.vue';
-import friendLinkPage from '@/components/body/friendLinkPage.vue';
-import musicPage from '@/components/body/musicPage.vue';
-import searchPage from '@/components/body/searchPage.vue';
-import bodyItem from '@/components/bodyItem.vue';
 import config from '@/config.json';
 export default {
   data() {
     return {
-      showBody: String,
-      showBodyDisplay: Boolean,
-      itemData: Object,
-      // 评论区开关，目前还没写
-      comment: Boolean,
       navigationBarData: config.navigationBarData,
       informationBarData: config.informationBarData,
     }
@@ -36,26 +23,11 @@ export default {
   components: {
     navigationBar,
     informationBar,
-    articlePage,
-    filePage,
-    aboutPage,
-    friendLinkPage,
-    musicPage,
-    searchPage,
-    bodyItem,
   }, methods: {
     handleClick(event) {
-      let defaultVue = ["articlePage", "searchPage", "musicPage", "filePage", "friendLinkPage", "aboutPage"]
-      this.itemData = this.navigationBarData.titleData[event.data].data;
-      this.comment = this.navigationBarData.titleData[event.data].comment;
-      if (defaultVue.includes(event.id)) {
-        this.showBodyDisplay = true;
-        this.showBody = event.id;
-      } else {
-        this.showBodyDisplay = false;
-      }
+      this.$router.push('/' + event.id);
     }
-  },
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import config from '../config.json'
+import { createRouter, createWebHistory } from 'vue-router';
+import config from '../config.json';
 
-// import navigationBar from '@/components/navigationBar.vue';
-// import informationBar from '@/components/informationBar.vue';
-// import articlePage from '@/components/body/articlePage.vue';
-// import filePage from '@/components/body/filePage.vue';
-// import aboutPage from '@/components/body/aboutPage.vue';
-// import friendLinkPage from '@/components/body/friendLinkPage.vue';
-// import musicPage from '@/components/body/musicPage.vue';
-// import searchPage from '@/components/body/searchPage.vue';
-import bodyItem from '@/components/bodyItem.vue';
+import bodyItem from '@/components/body/bodyItem.vue';
 
 const navigationBarData = config.navigationBarData;
 
 let defaultVue = ["articlePage", "searchPage", "musicPage", "filePage", "friendLinkPage", "aboutPage"];
 let pages = [];
 
-if (defaultVue.includes(navigationBarData.titleData[navigationBarData.defaultSelected - 1].id)) {
-    pages = [{ path: '/', component: () => import(`@/components/body/${navigationBarData.titleData[navigationBarData.defaultSelected - 1].id}.vue`) }];
+if (navigationBarData.titleData[navigationBarData.defaultSelected - 1].url === undefined && defaultVue.includes(navigationBarData.titleData[navigationBarData.defaultSelected - 1].id)) {
+    pages = [{
+        path: '/',
+        props: { itemData: navigationBarData.titleData[navigationBarData.defaultSelected - 1].data },
+        component: () => import(`@/components/body/${navigationBarData.titleData[navigationBarData.defaultSelected - 1].id}.vue`)
+    }];
 } else {
-    pages = [{ path: '/', component: bodyItem }];
+    pages = [{
+        path: '/',
+        props: { itemData: navigationBarData.titleData[navigationBarData.defaultSelected - 1].data },
+        component: bodyItem
+    }];
 }
 
-// 在此处可以使用 `pages`
-
-
-// 接下来可以在路由配置中使用`pages`
-
-
-// for (const allocation of navigationBarData.titleData) {
-//     if (defaultVue.includes(allocation.id)) {
-//         pages.push({ path: '/' + allocation.id, component: allocation.id });
-//     } else {
-//         pages.push({ path: '/' + allocation.id, component: bodyItem });
-//     }
-// }
 
 for (const allocation of navigationBarData.titleData) {
-    if (defaultVue.includes(allocation.id)) {
-        pages.push({ path: '/' + allocation.id, component: () => import(`@/components/body/${allocation.id}.vue`) });
+    if (allocation.url === undefined && defaultVue.includes(allocation.id)) {
+        pages.push({
+            path: '/' + allocation.id,
+            props: { itemData: allocation.data },
+            component: () => import(`@/components/body/${allocation.id}.vue`)
+        });
     } else {
-        pages.push({ path: '/' + allocation.id, component: bodyItem });
+        pages.push({
+            path: '/' + allocation.id,
+            props: { itemData: allocation.data },
+            component: bodyItem
+        });
     }
-}
 
-console.log(pages);
+}
 
 const router = createRouter({
     history: createWebHistory(),
